21ea2c28a683bee3b8846a240e8140b75253817a
ImalKesara <imalkesara2001@gmail.com>
2026-02-17 12:56:31 +0530

    feat: update dependencies and enhance layout with Navbar component
    - Added `@internationalized/date` and `bits-ui` to devDependencies.
    - Introduced a new `Navbar` component for improved navigation.
    - Updated `+layout.svelte` to include the `Navbar` and set a title.
    - Modified `layout.css` to incorporate the `Space Grotesk Variable` font and adjusted color variables.
    - Created a new `Separator` component for consistent UI separation.

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import { Moon } from 'lucide-svelte';
+</script>
+
+<header class="inset-x-2 top-0 left-0 z-20 bg-background text-sm">
+	<Separator />
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '$lib/components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Imal Kesara</title>
+</svelte:head>
+
+<!-- selection does highlights when you trying to select any text  -->
+<main class="selection:bg-foreground selection:text-background">
+	<Navbar />
+	{@render children()}
+</main>
