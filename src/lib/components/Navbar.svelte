29ce856294f355bcf320b73e96f750a326a9d46c
ImalKesara <imalkesara2001@gmail.com>
2026-02-19 01:36:43 +0530

    feat: enhance Navbar with theme toggle functionality and update layout styles

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import { Separator } from '$lib/components/ui/separator/index.js';
-	import { Moon, X, TextAlignEnd } from 'lucide-svelte';
+	import { Moon, X, TextAlignEnd, Monitor, Sun } from 'lucide-svelte';
 	import Container from './Container.svelte';
 	import { cn } from '$lib/utils';
 	import { page } from '$app/state';
 	import ThemeToggle from './Theme-toggle.svelte';
 	import { Button } from '$lib/components/ui/button';
+	import { resetMode, setMode, mode, theme } from 'mode-watcher';
+	import { fade } from 'svelte/transition';
 
 	const links = [
 		{ id: 0, label: 'Portfolio', href: '/' },
@@ -20,11 +22,15 @@
 		isMenuOpen = !isMenuOpen;
 	}
 
+	function handleOutsideClick(e: MouseEvent | KeyboardEvent) {
+		if (e.target !== e.currentTarget) return;
+		isMenuOpen = false;
+	}
+
 	$effect(() => {
 		currentPage = (
 			page.url.pathname.split('/')[1] ? `/${page.url.pathname.split('/')[1]}` : '/'
 		) as typeof currentPage;
-		console.log(currentPage);
 	});
 </script>
 
@@ -69,7 +75,53 @@
 					{/if}
 				</Button>
 			</div>
+
+			<!-- menu icon appearance -->
+			{#if isMenuOpen}
+				<div class="absolute top-12 right-4 z-20 w-58 flex-col border bg-background p-4 shadow-md">
+					<div class="flex items-center justify-center">
+						<span>Theme</span>
+						<Button
+							variant="ghost"
+							size="icon"
+							class="ml-2 cursor-pointer"
+							onclick={() => setMode('dark')}
+						>
+							<Moon />
+						</Button>
+
+						<Button
+							variant="ghost"
+							size="icon"
+							class="ml-2 cursor-pointer"
+							onclick={() => {
+								setMode('light');
+							}}
+						>
+							<Sun />
+						</Button>
+
+						<Button
+							variant="ghost"
+							size="icon"
+							class="cursor-pointer rounded-full"
+							onclick={() => {
+								resetMode();
+							}}><Monitor fill={mode.current == 'dark' ? 'white' : 'black'} /></Button
+						>
+					</div>
+				</div>
+			{/if}
+
+			{#if isMenuOpen}
+				<div
+					class="fixed top-0 left-0 z-10 h-full w-full bg-background/50 backdrop-blur-sm"
+					onclick={handleOutsideClick}
+					aria-hidden="true"
+					in:fade={{ duration: 200, delay: 100 }}
+					out:fade={{ duration: 200, delay: 100 }}
+				></div>
+			{/if}
 		</nav>
 	</Container>
-	<Separator />
 </header>
